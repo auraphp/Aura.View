8b0925f008385b33596fda1c55c5b0d2bcaed9d7
Hari K T <kthari85@gmail.com>
2011-12-13 23:04:44 +0530

    Adding missing docblock for docblox was placing on Default folder

--- src/Aura/View/HelperLocator.php
+++ src/Aura/View/HelperLocator.php
@@ -1,9 +1,39 @@
 <?php
+/**
+ * 
+ * This file is part of the Aura Project for PHP.
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
 namespace Aura\View;
+
+/**
+ * 
+ * The Aura View Helper Locator
+ * 
+ * @package Aura.View
+ * 
+ */
 class HelperLocator
 {
+    /**
+     * 
+     * A registry to store values
+     * 
+     * @var array
+     * 
+     */
     protected $registry;
     
+    /**
+     * 
+     * Constructor.
+     * 
+     * @param array $registry This will allow developers to use the DI system 
+     * of their choice to set up the helper locator.
+     * 
+     */
     public function __construct(array $registry = array())
     {
         foreach ($registry as $name => $spec) {
@@ -11,11 +41,28 @@ class HelperLocator
         }
     }
     
+    /*
+     * Set the registry with the name
+     * 
+     * @param string $name
+     * 
+     * @param string $spec
+     * 
+     * 
+     */
     public function set($name, $spec)
     {
         $this->registry[$name] = $spec;
     }
     
+    /*
+     * Gets from the registry 
+     * 
+     * @param string $name
+     * 
+     * @return string
+     * 
+     */
     public function get($name)
     {
         if (! isset($this->registry[$name])) {
