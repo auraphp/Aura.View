0221906042d70c3785e9b31730ba797c13b670aa
Paul M. Jones <pmjones88@gmail.com>
2011-12-26 12:28:47 -0600

    BREAKS, especially to terms: "view" is now "inner view", and "layout" is now "outer view".
    - [ADD] Property `$accept` to retain Accept values from a web context; add
      setAccept() and getAccept() methods.
    
    - [ADD] Property `$format` to retain which format is to be rendered; add
      setFormat() and getFormat() methods.
    
    - [BRK] Rename properties `$view_name`, `$view_data`, and `$view_paths` to
      `$inner_view`, `$inner_data`, and `$inner_paths` (respectively). Getter and
      setter methods have also been renamed.
    
    - [BRK] Rename properties `$layout_name`, `$layout_data`, and `$layout_paths`
      to `$outer_layout`, `$outer_data`, and `$outer_paths` (respectively). Getter
      and setter methods have also been renamed.
    
    - [BRK] Renamed `$layout_content_var` to `$inner_view_var`; default value is changed from `layout_content` to `inner_view`.
    
    - [CHG] Constructor now takes a `$format_types` param for a `FormatTypes`
      object.
    
    - [CHG] Inner and outer views may now be closures instead of template file
      names.
    
    - [CHG] Method `render()` now examines $accept and $format to choose an
      acceptable format for rendering.

--- src/Aura/View/TwoStep.php
+++ src/Aura/View/TwoStep.php
@@ -10,8 +10,8 @@ namespace Aura\View;
 
 /**
  * 
- * Provides a Two Step View pattern implementation; this allows for an 
- * inner "view" and and outer "layout" as commonly used by web apps.
+ * Provides a TwoStepView pattern implementation; this allows for an inner 
+ * ("core") view and and outer ("layout") view as commonly used by web apps.
  * 
  * @package Aura.View
  * 
@@ -20,72 +20,81 @@ class TwoStep
 {
     /**
      * 
-     * The name of the inner view template.
+     * An array of Accept headers, in the format ['type' => weight].
+     * 
+     * @var array
+     * 
+     */
+    protected $accept;
+    
+    /**
+     * 
+     * The .format to render.
      * 
      * @var string
      * 
      */
-    protected $view_name;
+    protected $format;
     
     /**
      * 
-     * The data for the inner view template.
+     * A FormatTypes object to map .format values to Content-Type values.
+     * 
+     * @var FormatTypes
+     * 
+     */
+    protected $format_types;
+    
+    /**
+     * 
+     * The data for the inner view.
      * 
      * @var array
      * 
      */
-    protected $view_data = array();
+    protected $inner_data = array();
     
     /**
      * 
-     * The paths to search when finding the inner view template.
+     * The paths to search when finding an inner view template.
      * 
      * @var array
      * 
      */
-    protected $view_paths = array();
+    protected $inner_paths = array();
     
     /**
      * 
-     * The name of the outer layout template.
+     * The inner view to use. This may be one of three types:
      * 
-     * @var string
+     * - (string) A template file name.
+     * 
+     * - (closure) A closure to execute. It should be of the format
+     * `function (...) uses (...) { ... };`.
+     * 
+     * - (array) An array where each element key is a .format name, and the
+     *   corresponding element value is a string or a closure. This type is
+     *   most useful when allowing for multiple views using the same data.
+     * 
+     * @var mixed
      * 
      */
-    protected $layout_name;
+    protected $inner_view;
     
     /**
      * 
-     * The data for the outer layout template.
-     * 
-     * @var array
-     * 
-     */
-    protected $layout_data = array();
-    
-    /**
-     * 
-     * The paths to search when finding the outer layout template.
-     * 
-     * @var array
-     * 
-     */
-    protected $layout_paths = array();
-    
-    /**
-     * 
-     * The name of the variable in the outer layout template that should be
+     * The name of the variable in the outer view template that should be
      * replaced with the output of the inner view template.
      * 
      * @var string
      * 
      */
-    protected $layout_content_var = 'layout_content';
+    protected $inner_view_var = 'inner_view';
     
     /**
      * 
      * The Template object to be used when rendering the inner view and outer
-     * layout.
+     * view.
      * 
      * @var Template
      * 
@@ -94,162 +103,485 @@ class TwoStep
     
     /**
      * 
+     * The data for the outer view.
+     * 
+     * @var array
+     * 
+     */
+    protected $outer_data = array();
+    
+    /**
+     * 
+     * The paths to search when finding the outer view template.
+     * 
+     * @var array
+     * 
+     */
+    protected $outer_paths = array();
+    
+    /**
+     * 
+     * The inner view to use. This may be one of three types:
+     * 
+     * - (string) A template file name.
+     * 
+     * - (closure) A closure to execute; it should be of the format
+     *   `function ($inner) uses (...) {...};` where `$inner` is the content 
+     *   of the inner view.
+     * 
+     * - (array) An array where each element key is a .format name, and the
+     *   corresponding element value is a string or a closure. This type is
+     *   most useful when allowing for multiple views using the same data.
+     * 
+     * @var mixed
+     * 
+     */
+    protected $outer_view;
+    
+    /**
+     * 
      * Constructor.
      * 
      * @param Template $template The Template object to be used when rendering
-     * the inner view and outer layout.
+     * the inner view and outer view.
      * 
      */
-    public function __construct(Template $template)
+    public function __construct(Template $template, FormatTypes $format_types)
     {
         $this->template = $template;
+        $this->format_types = $format_types;
     }
     
     /**
      * 
-     * Sets the name of the inner view template.
+     * Set the Accept values for negotiating formats.
      * 
-     * @param string $view_name The name of the inner view template.
+     * @param array $accept An array where each key is a content-type and each
+     * value is a corresponding weight.
      * 
      * @return void
      * 
      */
-    public function setViewName($view_name)
+    public function setAccept(array $accept = array())
     {
-        $this->view_name = $view_name;
+        $this->accept = $accept;
     }
     
     /**
      * 
-     * Sets the data for the inner view template.
+     * Returns the Accept values for negotiating formats.
      * 
-     * @param array $view_data The data for the inner view template.
+     * @return array
+     * 
+     */
+    public function getAccept()
+    {
+        return $this->accept;
+    }
+    
+    /**
+     * 
+     * Sets the .format to pick when the inner/outer views provide multiple
+     * formats.
+     * 
+     * @param string
      * 
      * @return void
      * 
      */
-    public function setViewData($view_data)
+    public function setFormat($format)
     {
-        $this->view_data = $view_data;
+        $this->format = $format;
+    }
+    
+    /**
+     * 
+     * Returns the .format to pick when the inner/outer views provide multiple
+     * formats.
+     * 
+     * @return string
+     * 
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+    
+    /**
+     * 
+     * Returns the Content-Type for the current .format, if any.
+     * 
+     * @return string
+     * 
+     */
+    public function getContentType()
+    {
+        return $this->format_types->getContentType($this->format);
+    }
+    
+    /**
+     * 
+     * Sets inner view specification. The specification may be:
+     * 
+     * - (string) A template file name.
+     * 
+     * - (closure) A closure to execute; it should be of the format
+     *   `function (...) uses (...) {...};`.
+     * 
+     * - (array) An array where each element key is a .format name, and the
+     *   corresponding element value is a string or a closure. This type is
+     *   most useful when allowing for multiple views using the same data.
+     * 
+     * @param mixed $inner_view The inner view specification.
+     * 
+     * @return void
+     * 
+     */
+    public function setInnerView($inner_view)
+    {
+        $this->inner_view = $inner_view;
+    }
+    
+    /**
+     * 
+     * Returns the inner view specification.
+     * 
+     * @param string $format Only return the specification for this .format;
+     * if null, return all formats.
+     * 
+     * @return mixed
+     * 
+     */
+    public function getInnerView($format = null)
+    {
+        return $this->getView($this->inner_view, $format);
+    }
+    
+    /**
+     * 
+     * Sets the data for the inner view.
+     * 
+     * @param array $inner_data The data for the inner view.
+     * 
+     * @return void
+     * 
+     */
+    public function setInnerData($inner_data)
+    {
+        $this->inner_data = $inner_data;
+    }
+    
+    /**
+     * 
+     * Returns the data for the inner view.
+     * 
+     * @param array $inner_data The data for the inner view.
+     * 
+     * @return void
+     * 
+     */
+    public function getInnerData()
+    {
+        return $this->inner_data;
     }
     
     /**
      * 
      * Sets the paths to search when finding the inner view template.
      * 
-     * @param string $view_paths The paths to search when finding the inner 
+     * @param array $inner_paths The paths to search when finding the inner 
      * view template.
      * 
      * @return void
      * 
      */
-    public function setViewPaths(array $view_paths = array())
+    public function setInnerPaths(array $inner_paths = array())
     {
-        $this->view_paths = $view_paths;
-    }
-    
-    public function addViewPath($path)
-    {
-        $this->view_paths[] = $path;
+        $this->inner_paths = $inner_paths;
     }
     
     /**
      * 
-     * Sets the name of the outer layout template.
+     * Appends a path to search when finding the inner view template.
      * 
-     * @param string $layout_name The name of the outer layout template.
+     * @param string $path The path to append.
      * 
      * @return void
      * 
      */
-    public function setLayoutName($layout_name)
+    public function addInnerPath($path)
     {
-        $this->layout_name = $layout_name;
+        $this->inner_paths[] = $path;
     }
     
     /**
      * 
-     * Sets the data for the outer layout template.
+     * Returns the paths to search when finding the inner view template.
      * 
-     * @param array $layout_data The data for the outer layout template.
+     * @return array
+     * 
+     */
+    public function getInnerPaths()
+    {
+        return $this->inner_paths;
+    }
+    
+    /**
+     * 
+     * Sets the outer view specification. The specification may be:
+     * 
+     * - (string) A template file name.
+     * 
+     * - (closure) A closure to execute; it should be of the format
+     *   `function ($inner) uses (...) {...};` where `$inner` is the content 
+     *   of the inner view.
+     * 
+     * - (array) An array where each element key is a .format name, and the
+     *   corresponding element value is a string or a closure. This type is
+     *   most useful when allowing for multiple views using the same data.
+     * 
+     * @param mixed $outer_view The outer view specification.
      * 
      * @return void
      * 
      */
-    public function setLayoutData($layout_data)
+    public function setOuterView($outer_view)
     {
-        $this->layout_data = $layout_data;
+        $this->outer_view = $outer_view;
     }
     
     /**
      * 
-     * Sets the paths to search when finding the outer layout template.
+     * Returns the outer view specification.
      * 
-     * @param string $layout_paths The paths to search when finding the outer 
-     * layout template.
+     * @param string $format Only return the specification for this .format;
+     * if null, return all formats.
+     * 
+     * @return mixed
+     * 
+     */
+    public function getOuterView($format = null)
+    {
+        return $this->getView($this->outer_view, $format);
+    }
+    
+    /**
+     * 
+     * Sets the data for the outer view.
+     * 
+     * @param array $outer_data The data for the outer view.
      * 
      * @return void
      * 
      */
-    public function setLayoutPaths(array $layout_paths = array())
+    public function setOuterData($outer_data)
     {
-        $this->layout_paths = $layout_paths;
-    }
-    
-    public function addLayoutPath($path)
-    {
-        $this->layout_paths[] = $path;
+        $this->outer_data = $outer_data;
     }
     
     /**
      * 
-     * Sets the name of the variable in the outer layout template that should 
+     * Returns the data for the outer view.
+     * 
+     * @return array
+     * 
+     */
+    public function getOuterData()
+    {
+        return $this->outer_data;
+    }
+    
+    /**
+     * 
+     * Sets the paths to search when finding the outer view template.
+     * 
+     * @param array $outer_paths The paths to search when finding the outer 
+     * view template.
+     * 
+     * @return void
+     * 
+     */
+    public function setOuterPaths(array $outer_paths = array())
+    {
+        $this->outer_paths = $outer_paths;
+    }
+    
+    /**
+     * 
+     * Appends a path to search when finding the outer view template.
+     * 
+     * @param string $path The path to append.
+     * 
+     * @return void
+     * 
+     */
+    public function addOuterPath($path)
+    {
+        $this->outer_paths[] = $path;
+    }
+    
+    /**
+     * 
+     * Returns the paths to search when finding the outer view template.
+     * 
+     * @return array
+     * 
+     */
+    public function getOuterPaths()
+    {
+        return $this->outer_paths;
+    }
+    
+    /**
+     * 
+     * Sets the name of the variable in the outer view template that should 
      * be replaced with the output of the inner view template.
      * 
-     * @param string $layout_content_var
+     * @param string $inner_view_var The variable name in the outer view
+     * template.
      * 
      * @return void
      * 
      */
-    public function setLayoutContentVar($layout_content_var)
+    public function setInnerViewVar($inner_view_var)
     {
-        $this->layout_content_var = $layout_content_var;
+        $this->inner_view_var = $inner_view_var;
     }
     
     /**
      * 
-     * Renders the inner view and outer layout templates and returns the
-     * resulting output.
+     * Returns the name of the variable in the outer view template that should 
+     * be replaced with the output of the inner view template.
+     * 
+     * @return string
+     * 
+     */
+    public function getInnerViewVar()
+    {
+        return $this->inner_view_var;
+    }
+    
+    /**
+     * 
+     * Renders the inner view and outer view and returns the resulting output,
+     * negotiating a format from the accept header values.
      * 
      * @return string The rendered two-step view results.
      * 
      */
     public function render()
     {
-        // no view? no layout? nothing to render.
-        if (! $this->view_name && ! $this->layout_name) {
-            return;
+        // if format is not set, pick a format to render based on Accept types
+        if (! $this->format && $this->accept) {
+            $accept = array_keys($this->accept);
+            $formats = array_keys((array) $this->inner_view);
+            $this->format  = $this->format_types->matchAcceptFormats(
+                $accept,
+                $formats
+            );
         }
         
-        // render the view and get its content for the layout
-        if ($this->view_name) {
-            $this->template->setData($this->view_data);
-            $this->template->setPaths($this->view_paths);
-            $content = $this->template->fetch($this->view_name);
-        } else {
-            $content = null;
+        // render inner view
+        $inner = $this->renderInnerView();
+        
+        // render outer view, and done
+        return $this->renderOuterView($inner);
+    }
+    
+    /**
+     * 
+     * Renders the inner view.
+     * 
+     * @return string
+     * 
+     */
+    public function renderInnerView()
+    {
+        $view = $this->getInnerView($this->format);
+        switch (true) {
+        case (! $view):
+            // no view
+            $inner = null;
+            break;
+        case ($view instanceof \Closure):
+            // view is a closure
+            $inner = $view();
+            break;
+        default:
+            // view is a string path to a template
+            $this->template->setData($this->inner_data);
+            $this->template->setPaths($this->inner_paths);
+            $inner = $this->template->fetch($view);
+            break;
+        }
+        return $inner;
+    }
+    
+    /**
+     * 
+     * Renders the outer view.
+     * 
+     * @param string $inner The output from the inner view.
+     * 
+     * @return string
+     * 
+     */
+    public function renderOuterView($inner)
+    {
+        $view = $this->getOuterView($this->format);
+        switch (true) {
+        case (! $view):
+            // no view
+            $outer = $inner;
+            break;
+        case ($view instanceof \Closure):
+            // view is a closure
+            $outer = $view($inner);
+            break;
+        default:
+            // view is a string path to a template
+            $this->outer_data[$this->inner_view_var] = $inner;
+            $this->template->addData($this->outer_data);
+            $this->template->setPaths($this->outer_paths);
+            $outer = $this->template->fetch($view);
+            break;
+        }
+        return $outer;
+    }
+    
+    /**
+     * 
+     * Gets the view for a particular format.
+     * 
+     * @param mixed $view The inner or outer view specification.
+     * 
+     * @return mixed The matching view for the format.
+     * 
+     */
+    protected function getView($view, $format)
+    {
+        // is the view empty?
+        if (! $view) {
+            return null;
         }
         
-        // render the layout and inject the view content.
-        // note that we *add* the layout data, which merges it with the
-        // previous view data instead of removing it.
-        if ($this->layout_name) {
-            $this->layout_data[$this->layout_content_var] = $content;
-            $this->template->addData($this->layout_data);
-            $this->template->setPaths($this->layout_paths);
-            return $this->template->fetch($this->layout_name);
-        } else {
-            return $content;
+        // is a format specified?
+        if ($format === null) {
+            return $view;
         }
+        
+        // is the view anything besides an array?
+        if (! is_array($view)) {
+            // not an array, return as-is
+            return $view;
+        }
+        
+        // the view is an array, look for a matching content-type
+        if (isset($view[$format])) {
+            return $view[$format];
+        }
+        
+        // no match
+        return false;
     }
 }
