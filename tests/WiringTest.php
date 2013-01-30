e1367ea78cdd4bcdc1484ba3aae7b4887373171d
Paul M. Jones <pmjones88@gmail.com>
2013-01-30 01:10:52 -0600

    add wiring test

---
+++ tests/WiringTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Aura\View;
+
+use Aura\Framework\Test\WiringAssertionsTrait;
+
+class WiringTest extends \PHPUnit_Framework_TestCase
+{
+    use WiringAssertionsTrait;
+
+    protected function setUp()
+    {
+        $this->loadDi();
+    }
+
+    public function testServices()
+    {
+    }
+
+    public function testInstances()
+    {
+        $this->assertNewInstance('Aura\View\Helper\Datetime');
+        $this->assertNewInstance('Aura\View\Helper\Escape');
+        $this->assertNewInstance('Aura\View\Template');
+        $this->assertNewInstance('Aura\View\TwoStep');
+    }
+    
+    public function testHelpers()
+    {
+        $helper = $this->assertNewInstance('Aura\View\HelperLocator');
+        $this->assertInstanceOf('Aura\View\Helper\Anchor',   $helper->get('anchor'));
+        $this->assertInstanceOf('Aura\View\Helper\Attribs',  $helper->get('attribs'));
+        $this->assertInstanceOf('Aura\View\Helper\Base',     $helper->get('base'));
+        $this->assertInstanceOf('Aura\View\Helper\Datetime', $helper->get('datetime'));
+        $this->assertInstanceOf('Aura\View\Helper\Escape',   $helper->get('escape'));
+        $this->assertInstanceOf('Aura\View\Helper\Image',    $helper->get('image'));
+        $this->assertInstanceOf('Aura\View\Helper\Links',    $helper->get('links'));
+        $this->assertInstanceOf('Aura\View\Helper\Metas',    $helper->get('metas'));
+        $this->assertInstanceOf('Aura\View\Helper\Scripts',  $helper->get('scripts'));
+        $this->assertInstanceOf('Aura\View\Helper\Scripts',  $helper->get('scriptsFoot'));
+        $this->assertInstanceOf('Aura\View\Helper\Styles',   $helper->get('styles'));
+        $this->assertInstanceOf('Aura\View\Helper\Title',    $helper->get('title'));
+    }
+}
