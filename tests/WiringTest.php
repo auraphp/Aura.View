7b0be2549d0a1097956a9b1ab2678895cddcc90f
Paul M. Jones <pmjones88@gmail.com>
2013-01-31 22:00:04 -0600

    remove 'label' helper; it's too simple

--- tests/WiringTest.php
+++ tests/WiringTest.php
@@ -30,7 +30,6 @@ class WiringTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Aura\View\Helper\Escape',   $helper->get('escape'));
         $this->assertInstanceOf('Aura\View\Helper\Image',    $helper->get('image'));
         $this->assertInstanceOf('Aura\View\Helper\Input',    $helper->get('input'));
-        $this->assertInstanceOf('Aura\View\Helper\Label',    $helper->get('label'));
         $this->assertInstanceOf('Aura\View\Helper\Links',    $helper->get('links'));
         $this->assertInstanceOf('Aura\View\Helper\Metas',    $helper->get('metas'));
         $this->assertInstanceOf('Aura\View\Helper\Radios',   $helper->get('radios'));
