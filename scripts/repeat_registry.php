5c5b3cc4721a3e0e124b0141b290c5ef44f8889f
Paul M. Jones <pmjones88@gmail.com>
2013-02-19 12:24:11 -0700

    add the repeat registry file

---
+++ scripts/repeat_registry.php
@@ -0,0 +1,7 @@
+<?php
+return array_merge(
+    require __DIR__ . '/field_registry.php',
+    [
+        'repeat'   => function () { return new Helper\Form\Repeat(require __DIR__ . '/field_registry.php') },
+    ]
+);
