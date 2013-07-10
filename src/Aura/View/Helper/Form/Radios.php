727615ee5e81d8d032eeaba677f7cc9681c5a115
Wietse Jorissen <wietse.jorissen@gmail.com>
2013-07-10 12:42:47 +0200

    Fixed a PSR error in Checkboxes.php, reset and branched for smarter development

--- src/Aura/View/Helper/Form/Radios.php
+++ src/Aura/View/Helper/Form/Radios.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * 
+ *
  * This file is part of the Aura Project for PHP.
- * 
+ *
  * @package Aura.View
- * 
+ *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- * 
+ *
  */
 namespace Aura\View\Helper\Form;
 
@@ -14,41 +14,41 @@ use Aura\View\Helper\AbstractHelper;
 use Aura\View\Helper\Form\Input\Checked;
 
 /**
- * 
+ *
  * Helper for series of `<input type="radio">` tags.
- * 
+ *
  * @package Aura.View
- * 
+ *
  */
 class Radios extends AbstractHelper
 {
     /**
-     * 
+     *
      * Constructor.
-     * 
+     *
      * @param Checked $input A checked-input helper for generating buttons.
-     * 
+     *
      */
     public function __construct(Checked $input)
     {
         $this->input = $input;
     }
-    
+
     /**
-     * 
+     *
      * Returns mulitple radio input fields.
-     * 
+     *
      * @param array $attribs The base attributes for the radios.
-     * 
+     *
      * @param array $options The radio values and labels.
-     * 
+     *
      * @param bool $checked Which radio value should be checked.
-     * 
+     *
      * @param string $separator The separator string to use between each
      * radio.
-     * 
+     *
      * @return string
-     * 
+     *
      */
     public function __invoke(
         $attribs,
@@ -66,34 +66,34 @@ class Radios extends AbstractHelper
         }
         return $html;
     }
-    
+
     /**
-     * 
+     *
      * Returns the input field HTML based on a field specification.
-     * 
+     *
      * @param array $spec A field specification.
-     * 
+     *
      * @return string
-     * 
+     *
      */
     public function getField($spec)
     {
         $attribs = isset($spec['attribs'])
                  ? $spec['attribs']
                  : [];
-        
+
         $options = isset($spec['options'])
                  ? $spec['options']
                  : [];
-        
+
         $value = isset($spec['value'])
                ? $spec['value']
                : null;
-               
+
         if (isset($spec['name'])) {
             $attribs['name'] = $spec['name'];
         }
-        
+
         return $this->__invoke($attribs, $options, $value);
     }
 }
