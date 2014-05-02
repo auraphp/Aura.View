7191b9be8150b19b485ea544e9c92505a5b9473d
Paul M. Jones <pmjones88@gmail.com>
2014-05-02 09:24:53 -0500

    fix autoload for 5.3

--- autoload.php
+++ autoload.php
@@ -17,10 +17,10 @@ spl_autoload_register(function ($class) {
     $part = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
     
     // directories where we can find classes
-    $dirs = [
+    $dirs = array(
         __DIR__ . DIRECTORY_SEPARATOR . 'src',
         __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'unit',
-    ];
+    );
     foreach ($dirs as $dir) {
         $file = $dir . DIRECTORY_SEPARATOR . $part;
         if (is_readable($file)) {
