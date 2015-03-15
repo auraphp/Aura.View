ffeb79fd960af1817b880b605bda32e0f79c7ab0
Paul M. Jones <pmjones88@gmail.com>
2015-03-15 12:44:05 -0500

    start testing finder functionality

--- tests/unit/src/TemplateRegistryTest.php
+++ tests/unit/src/TemplateRegistryTest.php
@@ -3,35 +3,32 @@ namespace Aura\View;
 
 class TemplateRegistryTest extends \PHPUnit_Framework_TestCase
 {
-    protected $template_registry;
-
-    protected function setUp()
-    {
-        $this->template_registry = new TemplateRegistry;
-    }
-
     public function testSetHasGet()
     {
+        $template_registry = new TemplateRegistry;
+
         $foo = function () {
             return "Foo!";
         };
 
-        $this->assertFalse($this->template_registry->has('foo'));
+        $this->assertFalse($template_registry->has('foo'));
 
-        $this->template_registry->set('foo', $foo);
-        $this->assertTrue($this->template_registry->has('foo'));
+        $template_registry->set('foo', $foo);
+        $this->assertTrue($template_registry->has('foo'));
 
-        $template = $this->template_registry->get('foo');
+        $template = $template_registry->get('foo');
         $this->assertSame($foo, $template);
 
         $this->setExpectedException('Aura\View\Exception\TemplateNotFound');
-        $this->template_registry->get('bar');
+        $template_registry->get('bar');
     }
 
     public function testSetString()
     {
-        $this->template_registry->set('foo', __DIR__ . '/foo_template.php');
-        $template = $this->template_registry->get('foo');
+        $template_registry = new TemplateRegistry;
+
+        $template_registry->set('foo', __DIR__ . '/foo_template.php');
+        $template = $template_registry->get('foo');
         $this->assertInstanceOf('Closure', $template);
 
         ob_start();
@@ -39,5 +36,31 @@ class TemplateRegistryTest extends \PHPUnit_Framework_TestCase
         $actual = ob_get_clean();
         $expect = 'Hello Foo!';
         $this->assertSame($expect, $actual);
+    }
+
+    public function testSetAndGetPaths()
+    {
+        $template_registry = new TemplateRegistry;
+
+        // should be no paths yet
+        $expect = array();
+        $actual = $template_registry->getPaths();
+        $this->assertSame($expect, $actual);
+
+        // set the paths
+        $expect = array('/foo', '/bar', '/baz');
+        $template_registry->setPaths($expect);
+        $actual = $template_registry->getPaths();
+        $this->assertSame($expect, $actual);
+    }
+
+    public function testSearch()
+    {
+        $template_registry = new TemplateRegistry;
+        $template_registry->fakefs = array(
+            '/foo',
+            '/bar',
+            '/baz'
+        );
     }
 }
